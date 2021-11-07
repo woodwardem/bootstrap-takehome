cdd46a6b8c7797868ad55cd3f9c72f0559d8b7e4
woodwardem <woodwardem@g.crookcountyschools.org>
2021-11-07 09:52:22 -0800

    assignemnt done for bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World!</p>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://freerangestock.com/photos/141161/perfect-blue-sky-background--with-fluffy-clouds.html" />
+          <Card.Body>
+            <Card.Title>High In The Sky!</Card.Title>
+            <Card.Text>
+              Here is my great text for this card. A pretty blue sky with big clouds!
+    </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
-    );
-  }
+  );
 }
+  }
 
 export default App;
