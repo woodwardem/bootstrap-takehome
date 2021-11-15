4aa3f3dc379cb1b491a0806f781af97faf16c2fd
woodwardem <woodwardem@g.crookcountyschools.org>
2021-11-14 18:24:29 -0800

    bootstrap takehome done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import "./App.css";
-
+import Form from 'react-bootstrap/Form';
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>UserName</Form.Label>
+            <Form.Control type="username" placeholder="username" />
+            <Form.Text className="text-muted">
+              We will take care of the private information you enter. 
+    </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Personal Code</Form.Label>
+            <Form.Control type="personal code" placeholder="code" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+  </Button>
+        </Form>
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src="https://freerangestock.com/photos/141161/perfect-blue-sky-background--with-fluffy-clouds.html" />
           <Card.Body>
@@ -18,8 +38,9 @@ class App extends React.Component {
           </Card.Body>
         </Card>
       </div>
-  );
-}
+    );
   }
+}
+
 
 export default App;
